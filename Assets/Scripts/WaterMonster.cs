f1ba5349a3b919e1948ecdbc83a84b45b715d0de
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2024-02-26 00:42:26 -0800

    add level 2 scene

--- Assets/Scripts/WaterMonster.cs
+++ Assets/Scripts/WaterMonster.cs
@@ -6,6 +6,27 @@ public class WaterMonster : MonoBehaviour
 {
     public int health = 2;
 
+    public float moveSpeed = 2f; // 移动速度
+    public float moveDistance = 2f; // 移动距离
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position; // 记录起始位置
+    }
+
+    void Update()
+    {
+        MoveVertically();
+    }
+
+    void MoveVertically()
+    {
+        Vector3 v = startPos;
+        v.y += Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+        transform.position = v;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
