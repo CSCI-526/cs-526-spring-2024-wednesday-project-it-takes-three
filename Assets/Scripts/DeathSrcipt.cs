9e4f6c2475fa2c352c4fe52dec59ae7f08684edb
Vicky <vickyc0320@gmail.com>
2024-03-04 14:07:32 -0800

    Fix bug on player death
    - the counter will reset following player's death

--- Assets/Scripts/DeathSrcipt.cs
+++ Assets/Scripts/DeathSrcipt.cs
@@ -1,38 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
 
-public class DeathSrcipt : MonoBehaviour
-{
-    public GameObject StartPoint;
-    public GameObject Player;
-    public TextMeshProUGUI fireCountText;
-    public TextMeshProUGUI waterCountText;
+//public class DeathSrcipt : MonoBehaviour
+//{
+//    public GameObject StartPoint;
+//    public GameObject Player;
+//    private PlayerCollect playerCollect;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        playerCollect = Player.GetComponent<PlayerCollect>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//    private void OnCollisionEnter2D(Collision2D other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            // Reset the player's position
+//            Player.transform.position = StartPoint.transform.position;
+//            Debug.Log("Player has died and will be reset.");
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Player.transform.position = StartPoint.transform.position;
-            Debug.Log("death");
+//            // Reset the element counts in PlayerCollect script
+//            playerCollect.fireElementCount = 0;
+//            playerCollect.waterElementCount = 0;
 
-            fireCountText.text = "Fire element count: 0" ;
-            waterCountText.text = "Water element count: 0";
-
-
-        }
-    }
-}
+//            // Update the UI text elements to reflect the new counts
+//            playerCollect.fireCountText.text = "Fire element count: " + playerCollect.fireElementCount;
+//            playerCollect.waterCountText.text = "Water element count: " + playerCollect.waterElementCount;
+//        }
+//    }
+//}
--- Assets/Scripts/FallingRespawn.cs
+++ Assets/Scripts/FallingRespawn.cs
@@ -8,6 +8,12 @@ public class FallingRespawn : MonoBehaviour
     public float threshold; // for Y value
     public GameObject StartPoint;
     public GameObject Player;
+    public PlayerCollect playerCollect;
+
+    void Start()
+    {
+        playerCollect = Player.GetComponent<PlayerCollect>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -16,6 +22,12 @@ public class FallingRespawn : MonoBehaviour
         {
             Player.transform.position = StartPoint.transform.position;
             Debug.Log("death");
+
+            playerCollect.fireElementCount = 0;
+            playerCollect.waterElementCount = 0;
+
+            playerCollect.fireCountText.text = "Fire element count: " + playerCollect.fireElementCount;
+            playerCollect.waterCountText.text = "Water element count: " + playerCollect.waterElementCount;
         }
 
     }
